d376dcc4b24eb228b5f02ec6dc4947cffb121cdc
Tanaya Solanke <solanketanaya@gmail.com>
2026-03-13 15:04:06 +0530

    ObserverDemo updated

---
+++ Day25/CommandPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿﻿
+Light light = new Light();
+// light.On();
+// light.Off();
+
+var lightOnCommand = new LightOnCommand(light);
+// lightOnCommand.Execute();
+
+var remoteControl = new RemoteControl(lightOnCommand);
+remoteControl.PressButton();
+
+public class Light
+{
+    public void On()
+    {
+        Console.WriteLine("Light is on");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("Light is off");
+    }
+}
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class LightOnCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOnCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.On();
+    }
+}
+
+// public class LightOffCommand : ICommand
+
+// Dispatcher
+public class RemoteControl
+{
+    private ICommand _command;
+
+    public RemoteControl(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void PressButton()
+    {
+        _command.Execute();
+    }
+}
+
+public class AlexaRemote
+{
+    public void VoiceCommands()
+    {
+        Console.WriteLine("Voice activated remote control");
+    }
+}
---
+++ Day25/ObserverDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿﻿
+
+var msft = new Stock() { Symbol = "MSFT" };
+
+var sarah = new User() { Name = "Sarah" };
+var bob = new User() { Name = "Bob" };
+
+msft.PriceChanged += sarah.PriceListener;
+msft.PriceChanged += bob.PriceListener;
+
+msft.Price = 350m;
+// msft.NotifyPriceChange();
+
+msft.PriceChanged -= sarah.PriceListener;
+
+msft.Price = 500m;
+// msft.NotifyPriceChange();
+
+public class User
+{
+    public string Name { get; set; }
+
+    public void PriceListener(Stock stock)
+    {
+        Console.WriteLine($"{Name} - Notification : Stock : {stock.Symbol} Price: {stock.Price:C2}");
+    }
+}
+
+
+public class Stock
+{
+    public string Symbol { get; set; }
+
+    decimal _price;
+    public decimal Price
+    {
+        get
+        {
+            return _price;
+        }
+        set
+        {
+            _price = value;
+            NotifyPriceChange();
+        }
+    }
+
+    public event Action<Stock> PriceChanged;
+
+
+    void NotifyPriceChange()
+    {
+        PriceChanged?.Invoke(this);
+    }
+}
